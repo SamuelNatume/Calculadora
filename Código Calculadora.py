6966d02dad4a5c682602ac458df5b30bccf2ad4e
SamuelNatume <saminatume@gmail.com>
2024-04-04 20:43:49 -0300

    .

--- Código Calculadora.py
+++ Código Calculadora.py
@@ -1,7 +1,3 @@
-# Adição
-def add(x, y):
-    return x + y
-
 # Subtração
 def subtract(x, y):
     return x - y
@@ -17,25 +13,28 @@ def divide(x, y):
     else:
         return x / y
 
-print("Selecione a operação:")
-print("1. Adição")
-print("2. Subtração")
-print("3. Multiplicação")
-print("4. Divisão")
-print("5. Sair")
+while True:
+    print("Selecione a operação:")
+    print("1. Adição")
+    print("2. Subtração")
+    print("3. Multiplicação")
+    print("4. Divisão")
+    print("5. Sair")
 
-# Escolha do usuário
-escolha = input("Digite sua escolha (1/2/3/4/5): ")
+    # Escolha do usuário
+    escolha = input("Digite sua escolha (1/2/3/4/5): ")
 
-if escolha == '5':
-    print("Você saiu!")
+    if escolha == '5':
+        print("Você saiu!")
+        break
 
-elif escolha in ('1', '2', '3', '4'):
+    elif escolha in ('1', '2', '3', '4'):
         num1 = float(input("Digite o primeiro número: "))
         num2 = float(input("Digite o segundo número: "))
+        
 
         if escolha == '1':
-            print(num1, "+", num2, "=", add(num1, num2))
+            print(num1, "+", num2, "=", num1 + num2)
 
         elif escolha == '2':
             print(num1, "-", num2, "=", subtract(num1, num2))
@@ -44,6 +43,11 @@ elif escolha in ('1', '2', '3', '4'):
             print(num1, "*", num2, "=", multiply(num1, num2))
 
         elif escolha == '4':
-            print(num1, "/", num2, "=", divide(num1, num2))
-else:
-    print("Opção inválida!") 
+            # Verifica se o segundo número é zero
+            if num2 == 0:
+                print("Erro! Divisão por zero não é permitida.")
+            else:
+                print(num1, "/", num2, "=", divide(num1, num2))
+    else:
+        print("Opção inválida!")
+        continue
